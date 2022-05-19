85a76257627f13f854e4fd63a8ef725ed77a0e1f
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-05-19 11:56:19 +0200

    Caisse

--- public/js/fin.js
+++ public/js/fin.js
@@ -1,25 +1,28 @@
 import * as personne from './clients.js'
 
-import * as garage from './garage.js'
+import {Lieux,caisse, Devis, pieces} from './garage.js'
 
 
-class Devis {
-    constructor(probleme, pieces, prix) {
-        this.probleme = probleme;
-        this.pieces = pieces;
-        this.prix = prix;
+// Payer la facture
+let i = 0;
+for (i; i< caisse.personnes.length ; i++){
+    if (caisse.personnes[i].argent < caisse.personnes[i].devis.prix ) {
+        console.log(`La voiture de ${caisse.personnes[i]} part à la casse. TRISTE.`)
+        // Partir
+        console.log(`${caisse.personnes[i].nom} est parti du garage A PIEDS et avec ${caisse.personnes[i].argent}€ dans son porte-monnaie.`)
+        caisse.personnes[i].splice(0,1)
+        i--;
+    } else {
+        // Payer la facture
+        caisse.personnes[i].argent -= caisse.personnes[i].devis.prix;
+        console.log(`${caisse.personnes[i].nom} a dépensé ${caisse.personnes[i].devis.prix} pour réparer sa voiture. Il lui reste ${caisse.personnes[i].argent}.`)
+        caisse.personnes[i].probleme = "";
+        // Partir
+        console.log(`${caisse.personnes[i].nom} est parti(e) du garage avec une VOITURE REPAREE.`)
+        caisse.personnes.splice(0,1)
+        i--;
     }
 }
 
-let rouleur = new Devis("rouleur", "rouleur neuf", 45);
-let pneu = new Devis("pneu crevé", "pneu neuf", 50);
-let feu = new Devis("feu avant cassé", "feu neuf", 60);
-let moteur = new Devis("moteur en panne", "moteur neuf", 800);
-let huile = new Devis("huile à moteur vide", "huile neuf", 100);
-
-let pieces = [rouleur, pneu, feu, moteur, huile]
 
 
-
-
-export{Devis, rouleur,pneu, feu, moteur, huile, pieces}
--- public/js/garage.js
+++ public/js/garage.js
@@ -30,7 +30,23 @@ attente.personnes = [personne.marcus, personne.julien, personne.lea, personne.se
 console.log(`Voici les clients présents dans la salle d'attente :`);
 console.table(attente.personnes)
 
-import {Devis, rouleur, pneu,feu, moteur, huile, pieces} from './fin.js'
+// Grille des devis
+class Devis {
+    constructor(probleme, pieces, prix) {
+        this.probleme = probleme;
+        this.pieces = pieces;
+        this.prix = prix;
+    }
+}
+
+let rouleur = new Devis("rouleur", "rouleur neuf", 45);
+let pneu = new Devis("pneu crevé", "pneu neuf", 50);
+let feu = new Devis("feu avant cassé", "feu neuf", 60);
+let moteur = new Devis("moteur en panne", "moteur neuf", 800);
+let huile = new Devis("huile à moteur vide", "huile neuf", 100);
+
+let pieces = [rouleur, pneu, feu, moteur, huile]
+
 
 // Qu'est-ce qui se passe dans le bureau?
 let i=0
@@ -44,7 +60,6 @@ for (i; i < attente.personnes.length ; i++) {
     console.table(attente.personnes);
     // Le client reçoit un devis 
     bureau.personnes[0].devis = pieces[i]
-    console.log(`${bureau.personnes[0].nom} a reçu un devis.`)
     // Le client donne 45€ au patron
     bureau.personnes[0].argent -= 45;
     console.log(`${bureau.personnes[0].nom} a donné 45€ pour le devis, il lui reste ${bureau.personnes[0].argent}€.`)
@@ -56,12 +71,18 @@ for (i; i < attente.personnes.length ; i++) {
     bureau.personnes.shift();
     // Il reste le patron dans le bureau (normalement)
     console.log(`Il reste dans le bureau : ${bureau.personnes[0].nom}`)
-    console.table(bureau.personnes)
     i--;
 }
 
-console.log(caisse.personnes)
+console.log(`Ces personnes attendent à la CAISSE :`)
+console.table(caisse.personnes)
+
+console.table(personne.marcus.devis)
+console.table(personne.julien.devis)
+console.table(personne.lea.devis)
+console.table(personne.serge.devis)
+console.table(personne.leo.devis)
 
 
 
-export {Lieux, attente, bureau, caisse}
+export {Lieux, caisse, attente, bureau, Devis, pieces}
